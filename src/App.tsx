cc3ce7e222ea7fe69e492fd8091a02a278474e5f
Simon <simon.vereecke@live.com>
2024-10-04 22:42:07 +0200

    add InfoSections

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import './App.css'
-import arrowIcon from './assets/icon-arrow.svg'
+import "./App.css";
+import arrowIcon from "./assets/icon-arrow.svg";
+import { InfoSection } from "./components/InfoSection";
 
 function App() {
   return (
-    <main>
+    <>
       <header>
-        <h1 className='app-title'>IP Address Tracker</h1>
+        <h1 className="app-title">IP Address Tracker</h1>
         <form>
-          <input type='text' placeholder='192.212.174.101' />
-          <button type='submit'><img src={arrowIcon} /></button>
+          <input type="text" placeholder="192.212.174.101" />
+          <button type="submit">
+            <img src={arrowIcon} />
+          </button>
         </form>
       </header>
-      <section>
-      </section>
-    </main>
-  )
+      <main>
+        <InfoSection label="IP ADDRESS" value="192.212.174.101" />
+        <InfoSection label="LOCATION" value="Brooklyn, NY 10001" />
+        <InfoSection label="TIMEZONE" value="UTC-05:00" />
+        <InfoSection label="ISP" value="SpaceX Starlink" />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/InfoSection.tsx
@@ -0,0 +1,13 @@
+type InfoSectionProps = {
+    label: string;
+    value: string;
+  };
+  
+  export function InfoSection({ label, value }: InfoSectionProps) {
+    return (
+      <section>
+        <h1 className="section-title">{label}</h1>
+        <p>{value}</p>
+      </section>
+    );
+  }
