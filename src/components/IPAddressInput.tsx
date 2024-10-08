928a0bb188d942ff3c7d43408230d2d567302461
Simon <simon.vereecke@live.com>
2024-10-08 22:59:41 +0200

    update input placeholder for mobile

--- src/components/IPAddressInput.tsx
+++ src/components/IPAddressInput.tsx
@@ -4,8 +4,12 @@ import "./IPAddressInput.css";
 export function IPAddressInput() {
   return (
     <form>
-      {/* <input type="text" placeholder="192.212.174.101" name="IP input" /> */}
-      <input type="text" placeholder="Search for any IP address or domain" name="IP input" />
+      <input
+        type="text"
+        placeholder="Search for any IP address or domain"
+        name="IP input"
+        className="mobile"
+      />
       <button type="submit">
         <img src={arrowIcon} />
       </button>
